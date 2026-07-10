1015580c91d35ba34e57e87cdd9440e4636e0637
Dhanus Chinnakalipatti Thilagaa Ramesh <ctrdhanus16122001@gmail.com>
2026-07-10 18:13:12 -0400

    Add files via upload

---
+++ protein_descriptors/LD/MCDfeat.m
@@ -0,0 +1,22 @@
+% clear all
+% clc
+% % [x,y]=fastaread('SC22000.txt');
+% load('N_protein_b.mat')
+
+% txt=N_protein_a;
+txt=N_protein_a;
+
+strn = regexprep(txt,'[XBJOUZ]','');
+num1=numel(strn);
+MCD_Pa=[];
+
+for i=1:num1
+    [M1]=MCDZD(strn{i});
+    M=[M1];
+    MCD_Pa=[MCD_Pa;M];
+    clear M;clear M1;
+end
+csvwrite('LD_N1.csv', MCD_Pa)
+% csvwrite('LD_N2.csv', MCD_Pa)
+% csvwrite('LD_P1.csv', MCD_Pa)
+% csvwrite('LD_P2.csv', MCD_Pa)
